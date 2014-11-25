5f55964c31609e5179ac8fe27a830e9621c5874d
Lakshmi <lakshmi.reguna@yahoo.com>
2014-11-25 00:21:07 -0800

    iTunes app

--- iTunesSearchApp/ViewController.swift
+++ iTunesSearchApp/ViewController.swift
@@ -8,18 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
 
+    @IBOutlet weak var appsTableView: UITableView!
+    
+    var tableData = []
+    
+    var api = APIController()
+    
+    let kCellIdentifier: String = "SearchResultCell"
+    
+    var imageCache = [String : UIImage]()
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
+        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.textLabel.text = rowData["trackName"] as? String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString: NSString = rowData["artworkUrl60"] as NSString
+        let imgURL: NSURL? = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        let imgData = NSData(contentsOfURL: imgURL!)
+        cell.imageView.image = UIImage(data: imgData!)
+        
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel?.text = formattedPrice
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Get the row data for the selected row
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        var name: String = rowData["trackName"] as String
+        var formattedPrice: String = rowData["formattedPrice"] as String
+        
+        var alert: UIAlertView = UIAlertView()
+        alert.title = name
+        alert.message = formattedPrice
+        alert.addButtonWithTitle("Ok")
+        alert.show()
+    }
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableData = resultsArr
+            self.appsTableView!.reloadData()
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        api.searchItunesFor("Taylor Swift")
+        self.api.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
